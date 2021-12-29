b0c1d86637a74c5c4eb0c49d26edc631a2be3bdc
christian aranda <kristian.aranda@gmail.com>
2021-12-29 21:29:32 +0100

    refactor

--- ChainOfResponsibility/Implementation/MultipleOfFiveHandler.cs
+++ ChainOfResponsibility/Implementation/MultipleOfFiveHandler.cs
@@ -6,12 +6,9 @@ namespace ChainOfResponsibility.Implementation
     {
         public override string? Run(int request)
         {
-            if (request.IsDivisibleBy(5))
-            {
-                return $"The number: {request} is divisible by 5";
-            }
-
-            return base.Run(request); ;
+            return request.IsDivisibleBy(5) ? 
+                $"The number: {request} is divisible by 5" : 
+                base.Run(request);
         }
     }
 }
--- ChainOfResponsibility/Implementation/MultipleOfThreeAndFiveHandler.cs
+++ ChainOfResponsibility/Implementation/MultipleOfThreeAndFiveHandler.cs
@@ -6,11 +6,8 @@ public class MultipleOfThreeAndFiveHandler : HandlerBase<int, string>
 {
     public override string? Run(int request)
     {
-        if (request.IsDivisibleBy(3) && request.IsDivisibleBy(5))
-        {
-            return $"The number: {request} is divisible by 3 and 5";
-        }
-
-        return base.Run(request);
+        return request.IsDivisibleBy(3) && request.IsDivisibleBy(5) ? 
+            $"The number: {request} is divisible by 3 and 5" : 
+            base.Run(request);
     }
 }
