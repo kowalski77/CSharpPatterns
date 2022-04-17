35e7cd55c568f854fb79a5b288ef2e17873409a3
christian aranda <kristian.aranda@gmail.com>
2022-04-17 17:25:40 +0200

    refactor maybe

--- CSharpPatterns.Tests/MaybeTests.cs
+++ CSharpPatterns.Tests/MaybeTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using SharedKernel.Functional;
 using Xunit;
 
@@ -7,16 +8,52 @@ namespace CSharpPatterns.Tests;
 public class MaybeTests
 {
     [Fact]
+    public void Constructor_defaults()
+    {
+        // Arrange
+        var maybe = new Maybe<Product>();
+
+        // Act
+        var getValue = () => maybe.Value;
+
+        // Assert
+        getValue.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void None_defaults()
+    {
+        // Arrange
+        Maybe<Product> maybe = Maybe.None;
+
+        // Act
+        var getValue = () => maybe.Value;
+
+        // Assert
+        getValue.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void Value_return_value_when_set()
+    {
+        // Arrange
+        var product = new Product();
+        Maybe<Product> maybeProduct = product;
+
+        // Act
+        var value = maybeProduct.Value;
+
+        // Assert
+        maybeProduct.HasValue.Should().BeTrue();
+        value.Should().Be(product);
+    }
+
+    [Fact]
     public void Match_unfolds_the_value()
     {
         // Arrange
-        const int id = 1;
-        const string name = "first product";
-        var maybe = (Maybe<Product>)new Product
-        {
-            Id = id,
-            Name = name
-        };
+        var product = new Product();
+        var maybe = (Maybe<Product>)product;
 
         // Act
         var dto = maybe.Match(item => new ProductDto
@@ -26,18 +63,18 @@ public class MaybeTests
         }, () => new ProductDto());
 
         // Assert
-        dto.Id.Should().Be(id);
-        dto.Name.Should().Be(name);
+        dto.Id.Should().Be(product.Id);
+        dto.Name.Should().Be(product.Name);
     }
 
     [Fact]
-    public void Match_returns_none_value_when_null_value()
+    public void Match_returns_other_value_when_null_value()
     {
         // Arrange
         const int id = 2;
         const string name = "none product";
 
-        Maybe<Product> maybe = (Product)null!;
+        Maybe<Product> maybe = null!;
 
         // Act
         var dto = maybe.Match(item => new ProductDto
@@ -59,13 +96,8 @@ public class MaybeTests
     public void Map_wraps_to_another_maybe()
     {
         // Arrange
-        const int id = 1;
-        const string name = "first product";
-        Maybe<Product> maybeProduct = new Product
-        {
-            Id = id,
-            Name = name
-        };
+        var product = new Product();
+        var maybeProduct = (Maybe<Product>)product;
 
         static ProductDto ToProductDto(Product item) => new()
         {
@@ -77,22 +109,17 @@ public class MaybeTests
 
         // Assert
         maybeProductDto.Should().BeOfType<Maybe<ProductDto>>();
-        var value = maybeProductDto.ValueOrThrow(string.Empty);
-        value.Id.Should().Be(id);
-        value.Name.Should().Be(name);
+        var value = maybeProductDto.Value;
+        value.Id.Should().Be(product.Id);
+        value.Name.Should().Be(product.Name);
     }
 
     [Fact]
     public void Bind_wraps_to_another_maybe()
     {
         // Arrange
-        const int id = 1;
-        const string name = "first product";
-        Maybe<Product> maybeProduct = new Product
-        {
-            Id = id,
-            Name = name
-        };
+        var product = new Product();
+        var maybeProduct = (Maybe<Product>)product;
 
         static Maybe<ProductDto> ToProductDto(Product item) => new ProductDto
         {
@@ -104,49 +131,17 @@ public class MaybeTests
 
         // Assert
         maybeProductDto.Should().BeOfType<Maybe<ProductDto>>();
-        var value = maybeProductDto.ValueOrThrow(string.Empty);
-        value.Id.Should().Be(id);
-        value.Name.Should().Be(name);
-    }
-
-    [Fact]
-    public void TryGetValue_returns_value()
-    {
-        // Arrange
-        var product = new Product
-        {
-            Id = 1, Name = "product"
-        };
-        Maybe<Product> maybeProduct = product;
-
-        // Act
-        var maybeValue = maybeProduct.TryGetValue(out var value);
-
-        // Assert
-        maybeValue.Should().BeTrue();
-        value.Should().Be(product);
-    }
-
-    [Fact]
-    public void TryGetValue_returns_false_when_no_value()
-    {
-        // Arrange
-        Maybe<Product> maybeProduct = (Product)null!;
-
-        // Act
-        var maybeValue = maybeProduct.TryGetValue(out var value);
-
-        // Assert
-        maybeValue.Should().BeFalse();
-        value.Should().BeNull();
+        var value = maybeProductDto.Value;
+        value.Id.Should().Be(product.Id);
+        value.Name.Should().Be(product.Name);
     }
 }
 
 public class Product
 {
-    public int Id { get; set; }
+    public int Id { get; set; } = 1;
 
-    public string? Name { get; set; }
+    public string? Name { get; set; } = "Product";
 }
 
 public class ProductDto
--- SharedKernel/Functional/Maybe.cs
+++ SharedKernel/Functional/Maybe.cs
@@ -1,24 +1,23 @@
 ﻿namespace SharedKernel.Functional;
 
-public class Maybe<T>
+[Serializable]
+public readonly struct Maybe<T> : IEquatable<Maybe<T>>
 {
-    private readonly bool hasValue;
-
     private readonly T value = default!;
-
-    protected Maybe()
-    {
-    }
 
     private Maybe(T value)
     {
         this.value = value;
-        this.hasValue = true;
+        this.HasValue = true;
     }
+
+    public T Value => this.ValueOrThrow();
+
+    public bool HasValue { get; }
 
     public TResult Match<TResult>(Func<T, TResult> some, Func<TResult> none)
     {
-        return this.hasValue ? some(this.value) : none();
+        return this.HasValue ? some(this.value) : none();
     }
 
     public static implicit operator Maybe<T>(T value)
@@ -26,46 +25,40 @@ public class Maybe<T>
         return value == null ? new Maybe<T>() : new Maybe<T>(value);
     }
 
-    public bool TryGetValue(out T tryValue)
+    public static implicit operator Maybe<T>(Maybe value)
     {
-        if (this.hasValue)
-        {
-            tryValue = this.value;
-            return true;
-        }
-
-        tryValue = default!;
-
-        return false;
+        return None;
     }
+
+    public static Maybe<T> None => new();
 
     public Maybe<TResult> Map<TResult>(Func<T, TResult> convert)
     {
-        return !this.hasValue ? new Maybe<TResult>() : convert(this.value);
+        return !this.HasValue ? new Maybe<TResult>() : convert(this.value);
     }
 
     public Maybe<TResult> Bind<TResult>(Func<T, Maybe<TResult>> convert)
     {
-        return !this.hasValue ? new Maybe<TResult>() : convert(this.value);
+        return !this.HasValue ? new Maybe<TResult>() : convert(this.value);
     }
 
     public Maybe<TResult> SelectMany<T2, TResult>(
         Func<T, Maybe<T2>> convert,
         Func<T, T2, TResult> finalSelect)
     {
-        if (!this.hasValue)
+        if (!this.HasValue)
         {
             return new Maybe<TResult>();
         }
 
         var converted = convert(this.value);
 
-        return !converted.hasValue ? new Maybe<TResult>() : finalSelect(this.value, converted.value);
+        return !converted.HasValue ? new Maybe<TResult>() : finalSelect(this.value, converted.value);
     }
 
     public Maybe<T> Where(Func<T, bool> predicate)
     {
-        if (!this.hasValue)
+        if (!this.HasValue)
         {
             return new Maybe<T>();
         }
@@ -75,27 +68,27 @@ public class Maybe<T>
 
     public T ValueOr(T defaultValue)
     {
-        return this.hasValue ? this.value : defaultValue;
+        return this.HasValue ? this.value : defaultValue;
     }
 
     public T ValueOr(Func<T> defaultValueFactory)
     {
-        return this.hasValue ? this.value : defaultValueFactory();
+        return this.HasValue ? this.value : defaultValueFactory();
     }
 
     public Maybe<T> ValueOrMaybe(Maybe<T> alternativeValue)
     {
-        return this.hasValue ? this : alternativeValue;
+        return this.HasValue ? this : alternativeValue;
     }
 
     public Maybe<T> ValueOrMaybe(Func<Maybe<T>> alternativeValueFactory)
     {
-        return this.hasValue ? this : alternativeValueFactory();
+        return this.HasValue ? this : alternativeValueFactory();
     }
 
-    public T ValueOrThrow(string errorMessage)
+    public T ValueOrThrow(string? errorMessage = null)
     {
-        if (this.hasValue)
+        if (this.HasValue)
         {
             return this.value;
         }
@@ -108,7 +101,33 @@ public class Maybe<T>
         throw new NotImplementedException();
     }
 
-    public class None : Maybe<T>
+    public bool Equals(Maybe<T> other)
     {
+        return this.HasValue == other.HasValue && EqualityComparer<T>.Default.Equals(this.value, other.value);
     }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Maybe<T> other && this.Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.HasValue, this.value);
+    }
+
+    public static bool operator ==(Maybe<T> left, Maybe<T> right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Maybe<T> left, Maybe<T> right)
+    {
+        return !left.Equals(right);
+    }
+}
+
+public struct Maybe
+{
+    public static Maybe None => new();
 }
