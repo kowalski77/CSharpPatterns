985beada142cd59374ed2939800c2d9bb90211be
Christian Aranda <kristian.aranda@gmail.com>
2022-06-25 17:27:44 +0200

    json tests

---
+++ JsonTests/Points.cs
@@ -0,0 +1,13 @@
+﻿namespace JsonTests;
+
+public class Point2D
+{
+    public double X { get; set; }
+
+    public double Y { get; set; }
+}
+
+public class Point3D : Point2D
+{
+    public double Z { get; set; }
+}
