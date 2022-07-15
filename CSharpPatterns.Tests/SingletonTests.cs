fb67be5e31b30aec1ad57b6f30d8fc15749a6460
Christian Aranda <kristian.aranda@gmail.com>
2022-07-15 20:42:47 +0200

    add singleton pattern

---
+++ CSharpPatterns.Tests/SingletonTests.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.Singleton;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpPatterns.Tests;
+
+public class SingletonTests
+{
+    [Fact]
+    public void Call_two_times_singleton_instance_returns_the_same_instance()
+    {
+        var logger1 = Logger.Instance;
+        var logger2 = Logger.Instance;
+
+        logger1.Should().Be(logger2);
+    }
+}
---
+++ DesignPatterns/Singleton/Logger.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Singleton;
+
+public class Logger
+{
+    private static readonly Lazy<Logger> lazyLogger = new Lazy<Logger>(() => new Logger());
+
+    private Logger() { }
+
+    public static Logger Instance => lazyLogger.Value;
+
+    public void Log(string message)
+    {
+        // ...
+    }
+}
