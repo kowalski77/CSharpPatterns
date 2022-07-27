6fdec87d671365ac9c4770e1c53c186f010f57cf
Christian Aranda <kristian.aranda@gmail.com>
2022-07-27 20:25:31 +0200

    EventHandler + tests

---
+++ CSharpPatterns.Tests/EventsTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Playground;
+using Xunit;
+
+namespace CSharpPatterns.Tests;
+
+public class EventsTests
+{
+    [Fact]
+    public void Callback_on_event_provides_reference_to_the_event_context()
+    {
+        // Arrange
+        const int id = 1;
+        var result = 0;
+
+        var bar = new Bar();
+        bar.BarExecuted += (sender, args) =>
+        {
+            result = args.Id;
+        };
+
+        // Act
+        bar.Execute(id);
+
+        // Assert
+        result.Should().Be(id);
+    }
+}
---
+++ Playground/Bar.cs
@@ -0,0 +1,21 @@
+﻿namespace Playground;
+
+public class Bar
+{
+    public void Execute(int id)
+    {
+        var args = new BarExecutedEventArgs
+        {
+            Id = id
+        };
+
+        this.BarExecuted?.Invoke(this, args);
+    }
+
+    public event EventHandler<BarExecutedEventArgs>? BarExecuted;
+}
+
+public class BarExecutedEventArgs : EventArgs
+{
+    public int Id { get; set; }
+}
