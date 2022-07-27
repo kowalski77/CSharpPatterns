e79ff6847291ebfac86f7988e634c31f63054fdb
Christian Aranda <kristian.aranda@gmail.com>
2022-07-27 20:09:23 +0200

    Add playground + Callback tests

---
+++ CSharpPatterns.Tests/CallbackTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Playground;
+using Xunit;
+
+namespace CSharpPatterns.Tests;
+
+public class CallbackTests
+{
+    [Fact]
+    public void Test()
+    {
+        // Arrange
+        var result = string.Empty;
+        var foo = new Foo();
+        foo.SimpleAction = (s) =>
+        {
+            result = s;
+        };
+
+        // Act
+        foo.RunSimpleAction();
+
+        // Assert
+        result.Should().Be("Hello World!");
+    }
+}
---
+++ Playground/Foo.cs
@@ -0,0 +1,11 @@
+﻿namespace Playground;
+
+public class Foo
+{
+    public Action<string>? SimpleAction { get; set; }
+
+    public void RunSimpleAction()
+    {
+        this.SimpleAction?.Invoke("Hello World!");
+    }
+}
