c9e9cb3e5a7cac0df6f3796a09cfd93dd392e865
Christian Aranda <kristian.aranda@gmail.com>
2022-07-28 20:22:38 +0200

    AddJsonConsole

--- LoggingDemo/Program.cs
+++ LoggingDemo/Program.cs
@@ -1,37 +1,43 @@
-﻿using LoggingDemo;
+﻿using System.Text.Json;
+using LoggingDemo;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Formatting.Compact;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .WriteTo.Console(new CompactJsonFormatter())
-    .CreateLogger();
+//Log.Logger = new LoggerConfiguration()
+//    .MinimumLevel.Information()
+//    .WriteTo.Console(new CompactJsonFormatter())
+//    .CreateLogger();
 
-using var loggerFactory = LoggerFactory.Create(builder => builder.AddSerilog(dispose: true));
+//using var loggerFactory = LoggerFactory.Create(builder => builder.AddSerilog(dispose: true));
+
+using var loggerFactory = LoggerFactory.Create(builder => builder.AddJsonConsole(options =>
+{
+    options.JsonWriterOptions = new JsonWriterOptions { Indented = true };
+}));
 
 var logger = loggerFactory.CreateLogger<Program>();
 
 var segment = new Segment(new Point(1, 2), new Point(4, 6));
 var length = segment.GetLength();
 
-Console.WriteLine("With string interpolation");
+logger.LogInformation("With string interpolation");
 // former, never use again $
 logger.LogInformation($"The length of segment {segment} is {length}");
 
-Console.WriteLine("With params");
+logger.LogInformation("With params");
 // former
 logger.LogInformation("The length of segment {@segment} is {length}.", segment, length);
 
-Console.WriteLine("With Serilog");
-// Serilog
-Log.Logger.Information("The length of segment {@segment} is {length}", segment, length);
+//Console.WriteLine("With Serilog");
+//// Serilog
+//Log.Logger.Information("The length of segment {@segment} is {length}", segment, length);
 
-Console.WriteLine("With Source Generators - Define");
+logger.LogInformation("With Source Generators - Define");
 // new
 logger.LogSegmentLength(segment, length);
 
-Console.WriteLine("With Source Generators - Interpolated string handler");
+logger.LogInformation("With Source Generators - Interpolated string handler");
 // improved
 logger.LogInterpolated(LogLevel.Information, $"The length of segment {segment} is {length}.");
 
