be4b4264628ef78736ec28917ba847fe259333ff
Christian Aranda <kristian.aranda@gmail.com>
2022-08-12 10:26:16 +0200

    logging scopes

--- LoggingDemo/Program.cs
+++ LoggingDemo/Program.cs
@@ -11,9 +11,15 @@ using Serilog.Formatting.Compact;
 
 //using var loggerFactory = LoggerFactory.Create(builder => builder.AddSerilog(dispose: true));
 
-using var loggerFactory = LoggerFactory.Create(builder => builder.AddJsonConsole(options =>
+//using var loggerFactory = LoggerFactory.Create(builder => builder.AddJsonConsole(options =>
+//{
+//    options.JsonWriterOptions = new JsonWriterOptions { Indented = true };
+//}));
+
+using var loggerFactory = LoggerFactory.Create(builder => builder.AddSimpleConsole(options =>
 {
-    options.JsonWriterOptions = new JsonWriterOptions { Indented = true };
+    options.IncludeScopes = true;
+    options.TimestampFormat = "hh:mm:ss ";
 }));
 
 var logger = loggerFactory.CreateLogger<Program>();
@@ -21,7 +27,7 @@ var logger = loggerFactory.CreateLogger<Program>();
 var segment = new Segment(new Point(1, 2), new Point(4, 6));
 var length = segment.GetLength();
 
-logger.LogInformation("With string interpolation");
+logger.LogInformation(AppLogEvents.Read, "With event id");
 // former, never use again $
 logger.LogInformation($"The length of segment {segment} is {length}");
 
@@ -40,5 +46,13 @@ logger.LogSegmentLength(segment, length);
 logger.LogInformation("With Source Generators - Interpolated string handler");
 // improved
 logger.LogInterpolated(LogLevel.Information, $"The length of segment {segment} is {length}.");
+
+
+var id = 3;
+using (logger.BeginScope("using block message"))
+{
+    logger.LogInformation(AppLogEvents.Read, "read value --> {id}", id);
+    logger.LogWarning(AppLogEvents.UpdateNotFound, "update value --> {id}", id);
+}
 
 Console.ReadKey();
