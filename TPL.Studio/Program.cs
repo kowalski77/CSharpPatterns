f23e8c0ba5360dcddcfb183042bea92f2398268d
Christian Aranda <kristian.aranda@gmail.com>
2022-08-14 20:32:09 +0200

    re

--- TPL.Studio/Program.cs
+++ TPL.Studio/Program.cs
@@ -1,23 +1,26 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿using System.Diagnostics;
 using TPL.Studio.Dataflow;
-
 
 //Why? Dataflow components are useful when you have multiple operations that must communicate with one another asynchronously or when you want to process data as it becomes available
 
-// await ExampleOne.ReverseWordsAsync();
+var sw = new Stopwatch();
+sw.Start();
+await ExampleOne.ReverseWordsAsync();
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
 
-var exampleTwo = new ExampleTwo();
-exampleTwo.CreateFile();
+//var exampleTwo = new ExampleTwo();
+//exampleTwo.CreateFile();
 
-var buffer = exampleTwo.CreateBuffer();
+//var buffer = exampleTwo.CreateBuffer();
 
-var newEntry = new CompetitionEntry
-{
-    Email = "random@email.com",
-    Answer = "a correct answer",
-    Created = DateTime.UtcNow,
-    IPAddress = "localhost"
-};
+//var newEntry = new CompetitionEntry
+//{
+//    Email = "random@email.com",
+//    Answer = "a correct answer",
+//    Created = DateTime.UtcNow,
+//    IPAddress = "localhost"
+//};
 
-await buffer.SendAsync(newEntry);
+//await buffer.SendAsync(newEntry);
 
