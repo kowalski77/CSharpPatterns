5859673b009ee43d32f71e3e53b4e76a9253cf2c
Christian Aranda <kristian.aranda@gmail.com>
2022-08-28 11:08:04 +0200

    create support method

--- DataRetrieval.Client/Program.cs
+++ DataRetrieval.Client/Program.cs
@@ -1,24 +1,3 @@
-﻿using System.Text.Json;
-using DataRetrieval.Client;
+﻿using DataRetrieval.Client;
 
-Console.WriteLine("Press a key to start...");
-Console.ReadKey();
-
-using HttpClient httpClient = new();
-
-using var response = await httpClient.GetAsync(new Uri("https://localhost:7206/People"), HttpCompletionOption.ResponseHeadersRead);
-
-response.EnsureSuccessStatusCode();
-
-var responseStream = await response.Content.ReadAsStreamAsync();
-var people = JsonSerializer.DeserializeAsyncEnumerable<Person>(responseStream, new JsonSerializerOptions
-{
-    PropertyNameCaseInsensitive = true
-});
-
-await foreach (var person in people!)
-{
-    Console.WriteLine($"{person!.Name} - {person.Email}");
-}
-
-Console.ReadKey();
+//await Scroll.RunAsync();
---
+++ DataRetrieval.Client/Scroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace DataRetrieval.Client;
+
+public static class Scroll
+{
+    public static async Task RunAsync()
+    {
+        using HttpClient httpClient = new();
+
+        using var response = await httpClient.GetAsync(new Uri("https://localhost:7206/People"), HttpCompletionOption.ResponseHeadersRead);
+
+        response.EnsureSuccessStatusCode();
+
+        var responseStream = await response.Content.ReadAsStreamAsync();
+        var people = JsonSerializer.DeserializeAsyncEnumerable<Person>(responseStream, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        await foreach (var person in people!)
+        {
+            Console.WriteLine($"{person!.Name} - {person.Email}");
+        }
+    }
+}
