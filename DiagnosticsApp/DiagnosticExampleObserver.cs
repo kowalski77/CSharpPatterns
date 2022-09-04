bb33687ff1a6cb3263684ab5fbf7832644a532dc
Christian Aranda <kristian.aranda@gmail.com>
2022-09-04 18:42:42 +0200

    DiagnosticExampleObserver

---
+++ DiagnosticsApp/DiagnosticExampleObserver.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace DiagnosticsApp;
+
+public class DiagnosticExampleObserver : IObserver<DiagnosticListener>
+{
+    public void OnCompleted() { }
+
+    public void OnError(Exception error) { }
+
+    public void OnNext(DiagnosticListener value)
+    {
+        if (value.Name == "HttpHandlerDiagnosticListener")
+        {
+            value.Subscribe(new HttpHandlerDiagnosticObserver());
+        }
+    }
+}
+
+public class HttpHandlerDiagnosticObserver : IObserver<KeyValuePair<string, object?>>
+{
+    private readonly AsyncLocal<Stopwatch> stopwatch = new();
+
+    public void OnCompleted() { }
+
+    public void OnError(Exception error) { }
+
+    public void OnNext(KeyValuePair<string, object?> value)
+    {
+        switch (value.Key)
+        {
+            case "System.Net.Http.Request":
+                PrintRequestInfo(value!);
+                this.stopwatch.Value = Stopwatch.StartNew();
+                break;
+            case "System.Net.Http.Response":
+                this.stopwatch.Value!.Stop();
+                Console.WriteLine("The HttpClient call took: " + this.stopwatch.Value.Elapsed.TotalMilliseconds + " milliseconds.");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static void PrintRequestInfo(KeyValuePair<string, object> value)
+    {
+        var type = value.Value.GetType();
+        var propInfo = type.GetProperty("Request");
+        var req = (HttpRequestMessage)propInfo!.GetValue(value.Value)!;
+
+        Console.WriteLine($"Uri: {req.RequestUri} - Method: {req.Method}");
+    }
+}
---
+++ DiagnosticsApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using DiagnosticsApp;
+using System.Diagnostics;
+
+Console.WriteLine("Press a key to retrieve products...");
+Console.ReadKey();  
+
+DiagnosticListener.AllListeners.Subscribe(new DiagnosticExampleObserver());
+
+HttpClient client = new();
+var response = await client.GetAsync("https://localhost:7215/products"); //Playground.Minimal.API
+response.EnsureSuccessStatusCode();
+
+var responseBody = await response.Content.ReadAsStringAsync();  
+
+Console.WriteLine(responseBody);
