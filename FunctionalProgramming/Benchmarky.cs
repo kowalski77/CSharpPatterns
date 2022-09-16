3f3ae747e20beaac1ab56b7fdfebdc704c6cade8
Christian Aranda <kristian.aranda@gmail.com>
2022-09-16 17:16:34 +0200

    update Benchmark

--- FunctionalProgramming/Benchmarky.cs
+++ FunctionalProgramming/Benchmarky.cs
@@ -6,29 +6,33 @@ namespace FunctionalProgramming;
 
 public class Benchmarky
 {
+    private static readonly Random rng = new(80085);
+
     private List<Product> productList = default!;
     private HashSet<Product> productSet = default!;
-    private Product targetProduct = default!;
 
     [GlobalSetup]
     public void GlobalSetup()
     {
         this.productList = ProductSeedData.GetRawProducts.Take(10_000).ToList();
         this.productSet = new HashSet<Product>(this.productList);
-        this.targetProduct = this.productList[4567];
     }
 
     [Benchmark]
     public Product ListFindProduct()
     {
-        return this.productList.First(x => x.Id == this.targetProduct.Id);
+        var targetProduct = this.productList[rng.Next(0, 9_999)];
+
+        return this.productList.First(x => x.Id == targetProduct.Id);
     }
 
     [Benchmark]
     public Product HashSetFindProduct()
     {
+        var targetProduct = this.productList[rng.Next(0, 9_999)];
+
         Product product;
-        this.productSet.TryGetValue(this.targetProduct, out product!);
+        this.productSet.TryGetValue(targetProduct, out product!);
 
         return product;
     }
