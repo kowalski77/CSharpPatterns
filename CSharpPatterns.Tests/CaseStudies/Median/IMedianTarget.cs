cc76ffbaf9494f94cbc773a398f8102ffb34696e
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-10-11 20:13:06 +0200

    Median Case Study

---
+++ CSharpPatterns.Tests/CaseStudies/Median/IMedianTarget.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpPatterns.Tests.CaseStudies.Median;
+
+public interface IMedianTarget<T> where T : IComparable<T>
+{
+    T GetMedian();
+}
+
+public class ListMedian<T> : IMedianTarget<T> where T : IComparable<T>
+{
+    public ListMedian(IEnumerable<T> content)
+    {
+        this.Content = content.ToList();
+        this.Sorted = new Lazy<List<T>>(() =>
+        {
+            this.Content.Sort();
+            return this.Content;
+        });
+    }
+
+    private List<T> Content { get; }
+
+    private Lazy<List<T>> Sorted { get; }
+
+    public T GetMedian() => this.Sorted.Value[this.Content.Count / 2];
+}
---
+++ CSharpPatterns.Tests/CaseStudies/Median/MedianTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+namespace CSharpPatterns.Tests.CaseStudies.Median;
+
+public abstract class MedianTests
+{
+    private static int[][] TestNumbers { get; } =
+    {
+        new[] { 2, 5, 1, 3, 4 }
+    };
+
+    public static IEnumerable<object[]> Data => TestNumbers.Select(args => new object[] { args });
+
+    protected abstract Func<IEnumerable<int>, IMedianTarget<int>> SutFactory { get; }
+
+    [Theory, MemberData(nameof(Data))]
+    public void Get_median_return_existing_value(int[] numbers) => numbers.Should().Contain(this.SutFactory(numbers).GetMedian());
+
+    [Theory, MemberData(nameof(Data))]
+    public void Get_median_separates_higher_half(int[] numbers) => (numbers.Length / 2).Should().BeLessThanOrEqualTo(numbers.Count(n => n >= this.SutFactory(numbers).GetMedian()));
+
+    [Theory, MemberData(nameof(Data))]
+    public void Get_median_separates_lower_half(int[] numbers) => (numbers.Length / 2).Should().BeLessThanOrEqualTo(numbers.Count(n => n <= this.SutFactory(numbers).GetMedian()));
+}
+
+public class ListMedianTests : MedianTests
+{
+    protected override Func<IEnumerable<int>, IMedianTarget<int>> SutFactory => numbers => new ListMedian<int>(numbers);
+}
