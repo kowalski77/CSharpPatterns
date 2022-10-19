544b6d15fd0f7ed69fb98b162bc771c18b993504
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-10-19 18:59:11 +0200

    CommonPerson tests

---
+++ CSharpPatterns.Tests/InstantiateTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Playground.Instantiate;
+
+namespace CSharpPatterns.Tests;
+
+public class InstantiateTests
+{
+    [Fact]
+    public void Copy_constructor_approach_creates_new_instance_with_new_properties()
+    {
+        // Arrange
+        const string oldName = "Jim";
+        const int oldAge = 68;
+        const string newName = "John";
+        const int newAge = 69;
+
+        CommonPerson commonPerson = new()
+        {
+            Name = oldName,
+            Age = oldAge,
+        };
+
+        // Act
+        CommonPerson sut = new(commonPerson)
+        {
+            Name = newName,
+            Age = newAge
+        };
+
+        // Assert
+        sut.Should().NotBe(commonPerson);
+        commonPerson.Name.Should().Be(oldName);
+        commonPerson.Age.Should().Be(oldAge);
+        sut.Name.Should().Be(newName);
+        sut.Age.Should().Be(newAge);
+    }
+}
---
+++ Playground/Instantiate/CommonPerson.cs
@@ -0,0 +1,31 @@
+﻿namespace Playground.Instantiate;
+
+// Immutable, cannot update properties without creating a new instance
+// For classes that does not want the ceremony on the constructor passing all the arguments.
+// init initialization
+public class CommonPerson
+{
+    public CommonPerson(string name = "", int age = 0)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    // Copy constructor instead With... approach
+    public CommonPerson(CommonPerson other)
+        : this(other.Name, other.Age)
+    {
+    }
+
+    public string Name { get; init; }
+
+    public int Age { get; init; }
+
+    // 1 drawback of With... approach: calling the constructor in many places inside the class itself
+    // If a new constructor parameter were added later, then we would have to modify all existing With
+    public CommonPerson WithName(string name) => new(name, this.Age);
+
+    // 2 drawback  of With... We can only change one property in one call. To modify several properties at once,
+    // we would have to change as many separate method calls and sustain construction of equally many new objects
+    public CommonPerson WithAge(int age) => new(this.Name, age);
+}
