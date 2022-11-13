813df4f8069cc437a166e152e526248b5913d42b
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-11-13 14:42:50 +0100

    new class access modifier in c#11: file

---
+++ Playground/FileModifier/File1.cs
@@ -0,0 +1,21 @@
+﻿namespace Playground.FileModifier;
+
+// In File1.cs:
+file interface IWidget
+{
+    int ProvideAnswer();
+}
+
+file class HiddenWidget
+{
+    public int Work() => 42;
+}
+
+public class Widget : IWidget
+{
+    public int ProvideAnswer()
+    {
+        var worker = new HiddenWidget();
+        return worker.Work();
+    }
+}
---
+++ Playground/FileModifier/File2.cs
@@ -0,0 +1,12 @@
+﻿namespace Playground.FileModifier;
+
+// In File2.cs:
+// Doesn't conflict with HiddenWidget 
+// declared in File1.cs
+public class HiddenWidget
+{
+    public void RunTask()
+    {
+        // omitted
+    }
+}
