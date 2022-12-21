a7f27bc282bbcc7786bcbd7c56a7913c2f14152a
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-12-21 20:56:09 +0100

    Advanced decorator

---
+++ CSharpPatterns.Tests/DecoratorAdvancedTests.cs
@@ -0,0 +1,49 @@
+﻿using DesignPatterns.Structural.DecoratorAdvanced;
+using FluentAssertions;
+
+namespace CSharpPatterns.Tests;
+
+public class DecoratorAdvancedTests
+{
+    [Fact]
+    public void Reverse_comparer_reverse_the_initial_list()
+    {
+        // Arrange
+        var list = new List<int> { 1, 2, 3, 4, 5 };
+        var comparer = Comparer<int>.Default;
+        
+        // Act
+        list.Sort(comparer.Reverse());
+
+        // Assert
+        list.Should().BeInDescendingOrder();
+    }
+
+    [Fact]
+    public void Two_chained_calls_to_reverse_comparer_reverse_the_initial_list()
+    {
+        // Arrange
+        var list = new List<int> { 1, 2, 3, 4, 5 };
+        var comparer = Comparer<int>.Default;
+
+        // Act
+        list.Sort(comparer.Reverse().Reverse());
+
+        // Assert
+        list.Should().BeInAscendingOrder();
+    }
+
+    [Fact]
+    public void Three_chained_calls_to_reverse_comparer_reverse_the_initial_list()
+    {
+        // Arrange
+        var list = new List<int> { 1, 2, 3, 4, 5 };
+        var comparer = Comparer<int>.Default;
+
+        // Act
+        list.Sort(comparer.Reverse().Reverse().Reverse());
+
+        // Assert
+        list.Should().BeInDescendingOrder();
+    }
+}
---
+++ DesignPatterns/Structural/DecoratorAdvanced/ReverseComparer.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.Structural.DecoratorAdvanced;
+
+file sealed class ReverseComparer<T> : IComparer<T>
+{
+    public readonly IComparer<T> other;
+
+    public ReverseComparer(IComparer<T> other) => this.other = other;
+
+    public int Compare(T? x, T? y) => this.other.Compare(y, x);
+}
+
+public static class ComparerFactory
+{
+    public static IComparer<T> Reverse<T>(this IComparer<T> other) =>
+        other is ReverseComparer<T> rc ?
+        rc.other :
+        new ReverseComparer<T>(other);
+}
