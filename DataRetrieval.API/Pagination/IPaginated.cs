151c7c45bd195c753c50dffd38cfb2a893b3f623
Christian Aranda <kristian.aranda@gmail.com>
2023-01-06 09:57:41 +0100

    refactor

--- DataRetrieval.API/Pagination/IPaginated.cs
+++
@@ -1,8 +0,0 @@
-namespace DataRetrieval.API.Pagination;
-
-public interface IPaginated<T> : IEnumerable<IPage<T>>
-{
-    int PagesCount { get; }
-
-    IPage<T> this[int offset] { get; }
-}
--- FunctionalProgramming/Pagination/IPaginated.cs
+++ FunctionalProgramming/Pagination/IPaginated.cs
@@ -3,6 +3,6 @@ namespace FunctionalProgramming.Pagination;
 public interface IPaginated<T> : IEnumerable<IPage<T>>
 {
     int PagesCount { get; }
-    
+
     IPage<T> this[int offset] { get; }
 }
