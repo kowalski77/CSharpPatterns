d2bb4800f3a4ac8e8078a8735b1fc0a57a1357ed
Christian Aranda <kristian.aranda@gmail.com>
2023-02-25 19:23:40 +0100

    Functional Discriminated unions and tests related

---
+++ CSharpPatterns.Tests/DiscriminatedUnionsTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using FunctionalProgramming.DiscriminatedUnions;
+
+namespace CSharpPatterns.Tests;
+
+public class DiscriminatedUnionsTests
+{
+    [Fact]
+    public void DiscreteMeasure_is_calculated_accordingly()
+    {
+        var m = new DiscreteMeasure("cm", 10);
+        var (a, b) = m.SplitInHalves();
+
+        a.Should().BeEquivalentTo(new DiscreteMeasure("cm", 5));
+        b.Should().BeEquivalentTo(new DiscreteMeasure("cm", 5));
+    }
+
+    [Fact]
+    public void ContinuousMeasure_is_calculated_accordingly()
+    {
+        var m = new ContinuousMeasure("cm", 10);
+        var (a, b) = m.SplitInHalves();
+
+        a.Should().BeEquivalentTo(new ContinuousMeasure("cm", 5));
+        b.Should().BeEquivalentTo(new ContinuousMeasure("cm", 5));
+    }
+}
---
+++ FunctionalProgramming/DiscriminatedUnions/MeasureTransforms.cs
@@ -0,0 +1,12 @@
+namespace FunctionalProgramming.DiscriminatedUnions;
+
+public static class MeasureTransforms
+{
+    public static (Measure a, Measure b) SplitInHalves(this Measure m) => m.MapAny(
+        d => (d with { Value = (d.Value + 1) / 2 }, d with { Value = d.Value / 2 }),
+        c =>
+        {
+            Measure half = c with { Value = c.Value / 2 };
+            return (half, half);
+        });
+}
