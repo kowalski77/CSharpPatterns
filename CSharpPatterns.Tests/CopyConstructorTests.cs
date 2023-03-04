77dc4fb03dfe909f2b9503f011bb217ec6082493
Christian Aranda <kristian.aranda@gmail.com>
2023-03-04 18:15:43 +0100

    copy constructors

---
+++ CSharpPatterns.Tests/CopyConstructorTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using FunctionalProgramming.NondestructiveMutation;
+
+namespace CSharpPatterns.Tests;
+
+public class CopyConstructorTests
+{
+    [Fact]
+    public void CopyConstructorTest()
+    {
+        AssemblySpecification original = new(Guid.NewGuid())
+        {
+            Name = "Original", 
+            Version = "1.0" 
+        };
+
+        AssemblySpecification copy = new(original)
+        {
+            Name = "Test"
+        };
+
+        copy.Id.Should().Be(original.Id);
+        copy.Name.Should().Be("Test");
+        copy.Version.Should().Be(original.Version);
+    }
+}
---
+++ FunctionalProgramming/CopyConstructor/AssemblySpecification.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FunctionalProgramming.NondestructiveMutation;
+
+// Copy constructor pattern (Nondestructive mutation), like "with" for records but for classes to achieve immutability.
+public class AssemblySpecification
+{
+    public AssemblySpecification(Guid id)
+    {
+        this.Id = id;
+    }
+
+    [SetsRequiredMembers] // this is dangerous, only use with copy constructors
+    public AssemblySpecification(AssemblySpecification other)
+        : this(other.Id)
+    {
+        this.Name = other.Name;
+        this.Version = other.Version;
+    }
+
+    public Guid Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required string Version { get; init; }
+}
