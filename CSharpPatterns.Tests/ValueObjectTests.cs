d0c25c36779016fa4c153fa9510346ebce687fbe
Christian Aranda <kristian.aranda@gmail.com>
2023-03-05 15:23:08 +0100

    Value object implementation

---
+++ CSharpPatterns.Tests/ValueObjectTests.cs
@@ -0,0 +1,79 @@
+﻿using Comparisons;
+using FluentAssertions;
+
+namespace CSharpPatterns.Tests;
+
+public class ValueObjectTests
+{
+    [Fact]
+    public void Same_values_are_equal()
+    {
+        var id = Guid.NewGuid();
+
+        OrderId orderId = new(id);
+        OrderId orderId1 = new(id);
+
+        orderId.Equals(orderId1).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Different_values_are_not_equal()
+    {
+        OrderId orderId = new(Guid.NewGuid());
+        OrderId orderId1 = new(Guid.NewGuid());
+
+        orderId.Equals(orderId1).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Different_types_are_not_equal()
+    {
+        var id = Guid.NewGuid();
+
+        OrderId orderId = new(id);
+        UserId userId = new(id);
+
+        orderId.Equals(userId).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Operators_works_correctly()
+    {
+        var id = Guid.NewGuid();
+        OrderId orderId = new(id);
+        OrderId orderId1 = new(id);
+
+        (orderId == orderId1).Should().BeTrue();    
+        (orderId != orderId1).Should().BeFalse();
+    }
+}
+
+file class OrderId : ValueObject2
+{
+    public Guid Id { get; }
+
+    public OrderId(Guid id)
+    {
+        this.Id = id;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return this.Id;
+    }
+}
+
+file class UserId : ValueObject2
+{
+    public Guid Id { get; }
+
+    public UserId(Guid id)
+    {
+        this.Id = id;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return this.Id;
+    }
+}
---
+++ Comparisons/ValueObject.cs
@@ -0,0 +1,39 @@
+﻿namespace Comparisons;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (GetType() != obj.GetType())
+            return false;
+
+        var valueObject = (ValueObject)obj;
+
+        return GetEqualityComponents()
+            .SequenceEqual(valueObject.GetEqualityComponents());
+    }
+
+    public override int GetHashCode() => GetEqualityComponents()
+            .Aggregate(1, (current, obj) =>
+            {
+                unchecked
+                {
+                    return current * 23 + obj.GetHashCode();
+                }
+            });
+
+    public static bool operator ==(ValueObject? a, ValueObject? b)
+    {
+        if (a is null && b is null)
+            return true;
+
+        return a is not null && b is not null && a.Equals(b);
+    }
+
+    public static bool operator !=(ValueObject? a, ValueObject? b) => !(a == b);
+}
